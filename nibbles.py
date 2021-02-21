dca31e9d26d0af72aabf5a08ae895e48d9f108f1
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-02-22 01:03:12 +1200

    implemented basic window

--- nibbles.py
+++ nibbles.py
@@ -4,7 +4,15 @@ import pygame
 
 pygame.init()
 
+#window settings
 window = pygame.display.set_mode((800, 800))
-pygame.display.update()
-pygame.quit()
-quit()
+pygame.display.set_caption('Nibbles')
+
+running = True
+while running:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.QUIT()
