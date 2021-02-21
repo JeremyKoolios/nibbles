d6ce79e4dfd399a15e14e498e3a1212ac3ad0945
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-02-22 02:57:37 +1200

    added wallhit gameOver condition

--- nibbles.py
+++ nibbles.py
@@ -9,6 +9,7 @@ window = pygame.display.set_mode((800, 800))
 pygame.display.set_caption('Nibbles')
 
 running = True
+gameOver = False
 
 cellSize = 50
 posx = (window.get_width() / 2) - cellSize
@@ -35,19 +36,25 @@ while running:
                 left, right, up, down = False, False, True, False
             elif event.key == pygame.K_DOWN:
                 left, right, up, down = False, False, False, True
-    if left == True:
+    if left:
         posx += -cellSize
-    elif right == True:
+    elif right:
         posx += cellSize
-    elif up == True:
+    elif up:
         posy += -cellSize
-    elif down == True:
+    elif down:
         posy += cellSize
     
+    #handles gameover conditions
+    if posx >= window.get_width() or posx < 0 or posy >= window.get_height() or posy < 0:
+        gameOver = True
+    
+    if gameOver:
+        break
+
     window.fill((0, 255, 255))
     pygame.draw.rect(window, (255, 0, 255), (posx, posy, cellSize, cellSize))
     pygame.display.update()
     pygame.time.delay(125)
-
 
 pygame.quit()
