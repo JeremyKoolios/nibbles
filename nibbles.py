08b794450300ded84f3722bb89714ecf95e344f3
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-02-22 22:14:40 +1200

    Update nibbles.py
    snake can now grow
    snake now moves like snake
    food can no longer generate inside snake
    snake can now collide with itself
    several blocks of code have been condensed into functions
    colors can now be changed through a global variable

--- nibbles.py
+++ nibbles.py
@@ -11,18 +11,37 @@ screenHeight = 800
 window = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Nibbles')
 
+#colors
+colorSnake = (0, 0, 255)
+colorApple = (255, 0, 0)
+colorBackground = (0, 255, 127)
+
 #game states
 running = True
 gameOver = False
 
 #game properties
 cellSize = 50
-posx = (screenWidth / 2) - cellSize
-posy = (screenHeight / 2) - cellSize
+growRate = 1 #controls how many cells the snake grows per apple
+
+posx = [(screenWidth / 2) - cellSize, (screenWidth / 2) - 2 * cellSize]
+posy = [(screenHeight / 2) - cellSize, (screenWidth / 2) - cellSize]
+
+currentScore = 0
+
 left = False
 right = False
 up = False
 down = False
+
+def drawSnake():
+    for i in range(len(posx)):
+        pygame.draw.rect(window, colorSnake, (posx[i], posy[i], cellSize, cellSize))
+
+def growSnake():
+    for i in range(growRate):
+        posx.append(posx[0])
+        posy.append(posy[0])
 
 def randCoord(screenWidth, screenHeight):
     randx = round(random.randrange(0, screenWidth - cellSize) / cellSize) * cellSize
@@ -31,9 +50,6 @@ def randCoord(screenWidth, screenHeight):
     return randx, randy
 
 foodx, foody = randCoord(screenWidth, screenHeight)
-
-currentScore = 0
-snakeLength = 1
 
 
 #infinitely loops wihle game is running
@@ -53,33 +69,50 @@ while running:
                 left, right, up, down = False, False, True, False
             elif event.key == pygame.K_DOWN:
                 left, right, up, down = False, False, False, True
+
+    #gets stnake moving in snake-like pattern
     if left:
-        posx += -cellSize
+        posx.pop()
+        posx.insert(0, posx[0] - cellSize)
+        posy.pop()
+        posy.insert(0, posy[0])
     elif right:
-        posx += cellSize
+        posx.pop()
+        posx.insert(0, posx[0] + cellSize)
+        posy.pop()
+        posy.insert(0, posy[0])
     elif up:
-        posy += -cellSize
+        posx.pop()
+        posx.insert(0, posx[0])
+        posy.pop()
+        posy.insert(0, posy[0] - cellSize)
     elif down:
-        posy += cellSize
+        posx.pop()
+        posx.insert(0, posx[0])
+        posy.pop()
+        posy.insert(0, posy[0] + cellSize)
     
     #handles gameover conditions
-    if posx >= screenWidth or posx < 0 or posy >= screenHeight or posy < 0:
+    if posx[0] >= screenWidth or posx[0] < 0 or posy[0] >= screenHeight or posy[0] < 0:
         gameOver = True
-    
+    for i in range(1, len(posx)):
+        if posx[0] == posx[i] and posy[0] == posy[i]:
+            gameOver = True
     if gameOver:
-        print('you died')
         break
 
     #handles food conditions
-    if posx == foodx and posy == foody:
-        snakeLength += 1
+    if posx[0] == foodx and posy[0] == foody:
+        growSnake()
         currentScore += 1
-        print('current score:', currentScore)
         foodx, foody = randCoord(screenWidth, screenHeight)
+        for i in range(len(posx)): #makes sure food does not generate inside of snake
+            while foodx == posx[i] and foody == posy[i]:
+                foodx, foody = randCoord(screenWidth, screenHeight)
 
-    window.fill((0, 255, 127))
-    pygame.draw.rect(window, (255, 0, 0), (foodx, foody, cellSize, cellSize)) #draw food
-    pygame.draw.rect(window, (0, 0, 255), (posx, posy, cellSize, cellSize)) #draw snake
+    window.fill(colorBackground)
+    pygame.draw.rect(window, colorApple, (foodx, foody, cellSize, cellSize)) #draw food
+    drawSnake() #draw snake
     pygame.display.update()
     pygame.time.delay(125)
 
